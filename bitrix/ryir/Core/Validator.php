f0475b59c6141596074205ee4cf239f79fd70849
nex641 <vviper5460@gmail.com>
2021-11-12 14:14:05 +0300

    Добавлена логика работы Валидатора

--- bitrix/ryir/Core/Validator.php
+++ bitrix/ryir/Core/Validator.php
@@ -69,29 +69,4 @@ class Validator
     public function in()
     {
     }
-    // $valid = new Validator('minLength', 5);
-    // $valid->exec($value) : boolean result
-    // или , когда сложная валидация
-    //
-    // private string Validator::type - тут будет хранится строковое представление метода валидации
-    // private ?mixed Validator::rule - правило для валидации
-    // private ?array Validator::validators- массив дополнительных объектов валидации
-
-    // public function exec() - метод выполнения валидации.
-    // public function {type}() - именованные методы валидации (например minLength() - проверка значения на длину, in() - проверка из списка, regexp())
-
-    // Как это должно работать.
-    // $valid = new Validator
-    //     (
-    //     'chain',
-    //      true,
-    //      [
-    //         new Validator('minLength', 5),
-    //         new Validator('regexp', '/^[A-Za-z0-9]{0,}$/'),
-    //         new Validator('email')
-    //      ]
-    //     );
-
-    // $valid->exec($value);
-
 }
